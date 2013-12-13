c165d252267ddf6507911dff7e661ce93d5d170c
mezbahalam <mezbahalam26@gmail.com>
2013-12-13 20:42:13 +0600

    baseline

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mezbah::Application.config.session_store :cookie_store, key: '_mezbah_session'
---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+Product.delete_all
+Product.create!(title: 'MezbahScript',
+  description: 
+    %{<p>
+        its my new.mezbahalam26@gmail.com
+      </p>},
+  image_url:   'cs.jpg',    
+  price: 36.00)
+# . . .
+Product.create!(title: 'Mezbah new products',
+  description:
+    %{<p>
+        oasjnahoiaebvoablkv savbaoivniaunv aivnaievniav viubaivn 
+      </p>},
+  image_url: 'ruby.jpg',
+  price: 49.95)
+# . . .
+
+Product.create!(title: 'Test Prescriptions',
+  description: 
+    %{<p>
+        inavo aoveahr gveavaenvuba oeivanlvknagea hvnadvnadvuiaebvgbeakv;lk
+        vjnvdvndevkdmnalkvmadm
+        vm;lmv oinvoenvoenv avnaoenvoanevo
+      </p>},
+  image_url: 'rtp.jpg',
+  price: 34.95)
